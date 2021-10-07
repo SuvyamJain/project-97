46fb8c7b18bffda01fd2acb290790c5b4a7ae5af
SuvyamJain <84795180+SuvyamJain@users.noreply.github.com>
2021-10-07 21:27:23 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,9 @@
+
+  function logout_1() {
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location.replace("login.html");
+    }
+
+    name_1 = 1;
+    
